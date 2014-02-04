8ca45f0e4fde619a66264affd7dd8fdb4f156642
ThunderGemios10 <marilyndris@gmail.com>
2014-02-04 20:42:24 +0800

    The Test Began

--- CustomCommands/src/herocc/bukkit/plugin/CustomCommands/Main.java
+++ CustomCommands/src/herocc/bukkit/plugin/CustomCommands/Main.java
@@ -8,6 +8,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 
 public class Main extends JavaPlugin {
+	
+	public static Main plugin;
+	public final Logger log = Logger.getLogger("Minecraft");
+	public String IN_GAME_PREFIX = "[CustomCommands] ";
+	public String PREFIX = "[CustomCommands] ";
+	
+	@Override
+	public void onEnable() {
+		log.info(PREFIX + is now Enabled);
+	}
+	
 
 	
 }
